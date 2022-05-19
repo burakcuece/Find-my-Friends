bc55d2152a1d86b03c1355f9eff3d1c9c6ef7e18
Burak <b.cuece@web.de>
2022-05-19 16:07:35 +0200

    Initial Commit

---
+++ Find my Friends/Find_my_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_my_FriendsApp.swift
+//  Find my Friends
+//
+//  Created by Burak Cüce on 19.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Find_my_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
